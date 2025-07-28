84da4805a7d125a6382a90c2b599bfe603ec2bda
prince joseph udoewah <princepiper28@gmail.com>
2025-07-28 03:41:33 +0100

    Initial commit for alx-project-0x04 inside alx-project-0x04-setup

---
+++ alx-project-0x04/pages/text-to-image.tsx
@@ -0,0 +1,9 @@
+const TextToImage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Text to Image Page</h1>
+    </div>
+  );
+};
+
+export default TextToImage;
